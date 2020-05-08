c676d9b63d28fdb0f2616b9e6af4e7aa505f9e57
Lars Hansen <lkh@dbc.dk>
2020-05-08 21:22:04 +0200

    Updated the settings.php file to match the files folder

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Composer template for Drupal 8 projects
+# Composer template for Drupal 8 projects.
 
 [![Build Status](https://travis-ci.org/drupal-composer/drupal-project.svg?branch=8.x)](https://travis-ci.org/drupal-composer/drupal-project)
 
--- docker/settings.php
+++ docker/settings.php
@@ -762,7 +762,9 @@ $settings['file_scan_ignore_directories'] = [
 # }
 
 $settings['install_profile'] = 'standard';
-$config_directories['sync'] = 'sites/default/files/config_YSgWpuwi8SMXMVbQbJobn_jZyTaEalX-WOJH0SbX1a4VT_VsO3qiwQphhNfCqUfpeCo4PpdcaQ/sync';
+$config_directories['sync'] = 'sites/default/files/config_pf0Fe8G8Z8BXloon8i_4RL5-2vDX580rxDkP1f8n00iM4scFcm336FxfUCVoO8BjC6pMPBZU0A/sync';
+$config_directories['deploy'] = '../config/sync';
+
 
 $conf['memcache_servers'] = array(
   '192.168.1.50:11211' => 'default'
